7f7d34eea1819485a003a0df0b9989864b6dfab5
TranDo25 <73243952+TranDo25@users.noreply.github.com>
2023-05-09 22:20:53 +0700

    comment some code

--- src/main/java/controller/SMSController.java
+++ src/main/java/controller/SMSController.java
@@ -4,27 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 import model.SMSSendRequest;
 import model.SmsPojo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.util.MultiValueMap;
+//import org.springframework.http.MediaType;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import service.SmsService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@RestController
-@RequestMapping("/api/v1")
+
+//@RequestMapping("/api/v1")
 @Slf4j
+@RestController
 public class SMSController {
 
     @Autowired
     SmsService service;
 
-    @Autowired
-    private SimpMessagingTemplate webSocket;
+//    @Autowired
+//    private SimpMessagingTemplate webSocket;
 
-    private final String  TOPIC_DESTINATION = "/lesson/sms";
+//    private final String  TOPIC_DESTINATION = "/lesson/sms";
 
 //    @RequestMapping(value = "/sms", method=RequestMethod.POST,
 //            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -46,13 +47,17 @@ public class SMSController {
         return service.sendSMS(sendRequest.getDestinationSMSNumber(), sendRequest.getSmsMessage());
 
     }
-
-    @RequestMapping(value = "/smscallback", method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public void smsCallback(@RequestBody MultiValueMap<String, String> map) {
-        service.receive(map);
-        webSocket.convertAndSend(TOPIC_DESTINATION, getTimeStamp() + ": Twilio has made a callback request! Here are the contents: "+map.toString());
+    @GetMapping("/")
+    public String index(){
+        return "hello world";
     }
+
+//    @RequestMapping(value = "/smscallback", method = RequestMethod.POST,
+//            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void smsCallback(@RequestBody MultiValueMap<String, String> map) {
+//        service.receive(map);
+//        webSocket.convertAndSend(TOPIC_DESTINATION, getTimeStamp() + ": Twilio has made a callback request! Here are the contents: "+map.toString());
+//    }
 
     private String getTimeStamp() {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime .now());
--- src/main/java/model/SMSSendRequest.java
+++ src/main/java/model/SMSSendRequest.java
@@ -1,27 +1,15 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SMSSendRequest {
     private String destinationSMSNumber;
     private String smsMessage;
 
-    public SMSSendRequest(String destinationSMSNumber, String smsMessage) {
-        this.destinationSMSNumber = destinationSMSNumber;
-        this.smsMessage = smsMessage;
-    }
 
-    public String getDestinationSMSNumber() {
-        return destinationSMSNumber;
-    }
-
-    public void setDestinationSMSNumber(String destinationSMSNumber) {
-        this.destinationSMSNumber = destinationSMSNumber;
-    }
-
-    public String getSmsMessage() {
-        return smsMessage;
-    }
-
-    public void setSmsMessage(String smsMessage) {
-        this.smsMessage = smsMessage;
-    }
 }
